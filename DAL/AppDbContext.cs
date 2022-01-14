56f89b39dd5ab5cb8198998b47d6bad886fbd9cb
Keijo Raamat <raamatkeijo@gmail.com>
2022-01-14 10:30:14 +0200

    Adds data access layer

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAL;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+    {
+    }
+
+
+    // public DbSet<Recipe> Recipes { get; set; } = default!;
+    // public DbSet<Ingredient> Ingredients { get; set; } = default!;
+    // public DbSet<IngredientInRecipe> IngredientInRecipes { get; set; } = default!;
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder){
+        base.OnModelCreating(modelBuilder);
+
+        foreach (var relationship in modelBuilder.Model
+                     .GetEntityTypes()
+                     .Where(e => !e.IsOwned())
+                     .SelectMany(e => e.GetForeignKeys()))
+        {
+            relationship.DeleteBehavior = DeleteBehavior.Restrict;
+        }
+    }
+}
